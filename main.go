d7c92877c81ff5293b201020b541cd0a6aeb79e4
h18verybody <45327349+h18verybody@users.noreply.github.com>
2022-12-24 11:11:52 +0300

    Update main.go
    change 13 line

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 
 // RootHandler is mapping "/"
 func RootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello gorilla/mux world!")
+	fmt.Fprintf(w, "hola")
 }
 
 // GetSampleHandler is mapping "/sample"
